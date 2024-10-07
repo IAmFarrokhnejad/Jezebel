24e8d958cfc7cf11c4505f1d64697eb7e1f5166a
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-10-07 17:24:52 +0300

    Update Jezebel 0.3.py

--- Jezebel 0.3.py
+++ Jezebel 0.3.py
@@ -11,7 +11,7 @@ import time
 import openai
 import tkinter as tk
 
-openai.api_key = "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"  # openai.apikey
+openai.api_key = "KEY GOES HERE"  # OPENAI API KEY GOES HERE
 
 engine = pyttsx3.init()
 
